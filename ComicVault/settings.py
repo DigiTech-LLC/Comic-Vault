cfe382c9eac69c32227b09024241512838c44414
John Berkley <jberkley95@gmail.com>
2018-04-30 15:45:43 -0700

    Made Secret Key secret again

--- ComicVault/settings.py
+++ ComicVault/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0*&4ewow0+97cdy9pa%&88^c+5s5l+@^11!fp9nba9s67%r@nk'
+SECRET_KEY = SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
