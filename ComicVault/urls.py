cd916b9da04f184dd9845fbd7a6bb9c724f986f3
P330 <wu-jason@sbcglobal.net>
2018-05-01 13:20:06 -0700

    import fix

--- ComicVault/urls.py
+++ ComicVault/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.conf.urls import include, path
 
 urlpatterns = [
     path('', include('Vault.urls')),
