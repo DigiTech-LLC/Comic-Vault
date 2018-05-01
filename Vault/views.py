3ff2bed9f5bf91e789e8503b6e694df93116221b
P330 <wu-jason@sbcglobal.net>
2018-05-01 16:22:31 -0700

    Test upload during host

--- Vault/views.py
+++ Vault/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the ComicVault Homepage.")
+    return HttpResponse("Hello, world. You're at the ComicVault Homepage. Test upload during host")
