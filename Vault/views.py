53b6336360e3b5c949da28af34c4f646a39576d9
Amanda Garcia <amgarcia2996@gmail.com>
2018-05-01 21:15:59 -0700

    Update views.py

--- Vault/views.py
+++ Vault/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the ComicVault Homepage. Test upload during host")
+    return HttpResponse("Hello, world. You're at the ComicVault Homepage. Test upload during host. Testing 1,2,3!!!")
