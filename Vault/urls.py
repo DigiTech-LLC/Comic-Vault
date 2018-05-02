ae5281fe7980e75e19b524427178df9642c18a07
P330 <wu-jason@sbcglobal.net>
2018-05-01 21:21:59 -0700

    test comic page

--- Vault/urls.py
+++ Vault/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # [url]/
     path('', views.index, name='index'),
+    # [url]/comic/[comic_id]
+    path('comic/<int:id>/', views.comicpage, name='comicpage'),
 ]
--- Vault/views.py
+++ Vault/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the ComicVault Homepage. Test upload during host")
+
+def comicpage(request, id):
+    response = "comic page lolololol"
+    return HttpResponse(response % id)
