5062cb0ab2807826d4a6f787976b781c72416c52
P330 <wu-jason@sbcglobal.net>
2018-05-01 22:27:16 -0700

    fixed argument

--- Vault/views.py
+++ Vault/views.py
@@ -5,5 +5,5 @@ def index(request):
     return HttpResponse("Hello, world. You're at the ComicVault Homepage. Test upload during host. Testing 1,2,3!!!")
 
 
-def comicpage(request):
+def comicpage(request, id):
     return HttpResponse("pls work %s." % question_id)
