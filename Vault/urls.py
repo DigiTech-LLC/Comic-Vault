631199df395de3d826b2ceadf4b0fe5b5e4bada5
John Berkley <jberkley95@gmail.com>
2018-05-01 23:39:51 -0700

    Added timeline page, added functionality to read user comments from database

--- Vault/urls.py
+++ Vault/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     # [url]/
     path('', views.index, name='index'),
 
-    # [url]/comic_id
+    # [url]/comic/comic_id
     path('comic/<int:id>/', views.comicpage, name='comicpage'),
+
+    # [url]/timeline/user_profile_id
+    path('timeline/<int:id>/', views.timeline, name='timeline')
 ]
--- Vault/views.py
+++ Vault/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import TimelinePost, TimelineComment
 
 
 def index(request):
@@ -7,3 +10,14 @@ def index(request):
 
 def comicpage(request, id):
     return HttpResponse("pls work %s." % id)
+
+
+def timeline(request, id):
+    timeline_post_list = TimelinePost.objects.all().filter(user_profile_id=id)
+    timeline_comment_list = TimelineComment.objects.all()
+    template = loader.get_template('Vault/timeline.html')
+    context = {
+        'timeline_post_list': timeline_post_list,
+        'timeline_comment_list': timeline_comment_list
+    }
+    return HttpResponse(template.render(context, request))
